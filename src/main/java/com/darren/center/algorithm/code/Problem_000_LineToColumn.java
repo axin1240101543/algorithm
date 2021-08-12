a0aa59af47b2cd1d854c1922ed3b5c8d2598f0af
龚德新 <darren23@sfmail.sf-express.com>
2021-08-12 17:28:31 +0800

    行转列

---
+++ src/main/java/com/darren/center/algorithm/code/Problem_000_LineToColumn.java
@@ -0,0 +1,34 @@
+package com.darren.center.algorithm.code;
+
+/**
+ * Author: 01403292
+ * Date: 2021-08-12 10:36:46
+ * Version: 1.0
+ * Description:
+ */
+public class Problem_000_LineToColumn {
+
+    public static void main(String[] args) {
+        int[][] nums = {{ 1, 2, 3}, {4,5,6}, {7,8,9} };
+        int[][] solution = solution(nums);
+        System.out.println();
+    }
+
+    private static int[][] solution(int[][] nums){
+        if (nums == null || nums.length <= 0){
+            return null;
+        }
+        int[] c1 = new int[3];
+        int[] c2 = new int[3];
+        int[] c3 = new int[3];
+        for (int i = 0; i < nums.length; i++){
+            int[] temp = nums[i];
+            int a = 0;
+            c1[i] = temp[a];
+            c2[i] = temp[++a];
+            c3[i] = temp[++a];
+        }
+        int[][] result = {c1, c2, c3};
+        return result;
+    }
+}
