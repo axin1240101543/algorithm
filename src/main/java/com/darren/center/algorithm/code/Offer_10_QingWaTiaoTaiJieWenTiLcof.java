cd7b58d98a418c40c8cf7aea768d49bc11a9c578
龚德新 <darren23@sfmail.sf-express.com>
2021-08-17 14:12:58 +0800

    剑指 Offer 10- II. 青蛙跳台阶问题

---
+++ src/main/java/com/darren/center/algorithm/code/Offer_10_QingWaTiaoTaiJieWenTiLcof.java
@@ -0,0 +1,25 @@
+package com.darren.center.algorithm.code;
+
+/**
+ * Author: Darren
+ * Date: 2021-08-16 10:21:11
+ * Version: 1.0
+ * Description:
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ * 输入：n = 0
+ * 输出：1
+ * 0 <= n <= 100
+ */
+public class Offer_10_QingWaTiaoTaiJieWenTiLcof {
+
+    public static void main(String[] args) {
+        Solution solution = new Offer_10_QingWaTiaoTaiJieWenTiLcof().new Solution();
+    }
+    class Solution {
+        public int numWays(int n) {
+            return 0;
+        }
+    }
+
+}
