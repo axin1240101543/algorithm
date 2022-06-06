4eb0b2bdefb053a8ff0a97dce0ae8b023f5596d6
Darren <1240101543@qq.com>
2022-06-06 22:20:53 +0800

    归并排序 非递归版

--- src/main/java/com/darren/center/algorithm2/class04/Code01_MergeSort.java
+++ src/main/java/com/darren/center/algorithm2/class04/Code01_MergeSort.java
@@ -55,6 +55,50 @@ import com.darren.center.algorithm2.class01.LogarithmMachine;
  * 为什么归并排序比选择排序、冒泡排序、插入排序好？
  * 因为他们这些排序大量浪费比较行为
  *
+ * 迭代版本：
+ * 3，2，1，4，6，0，7，5
+ * 准备一个变量，步长1 2 4 8 16 ... N-1
+ * 步长为1
+ * 左组：3
+ * 右组：2
+ * merge：2，3
+ * ------
+ * 左组：1
+ * 右组：4
+ * merge：1，4
+ * ------
+ * 左组：6
+ * 右组：0
+ * merge：0，6
+ * ------
+ * 左组：7
+ * 右组：5
+ * merge：5，7
+ * -----------------
+ * 步长为2
+ * 左组：2，3
+ * 右组：1，4
+ * merge：1，2，3，4
+ * ------
+ * 左组：0，6
+ * 右组：5，7
+ * merge：0，5，6，7
+ * -----------------
+ * 步长为4
+ * 左组：1，2，3，4
+ * 右组：0，5，6，7
+ * merge：0，1，2，3，4，5，6，7
+ *
+ * 左组不够或者没有右组，直接跳过
+ *
+ * 边界：
+ * 1、步长小于N，超过N结束排序
+ * 2、L（左组第一个位置）小于N，超过N本次步长结束
+ * 3、M（左组最后一个位置）大于等于N，跳过，说明左组不够
+ * 4、R（右组最后一个位置） = M + 1 + 步长  或者 N - 1(不够)
+ * 5、步长大于N/2，跳出，防止溢出
+ * 6、L ... M，M + 1... R﻿​
+ *
  * @author : Darren
  * @date : 2022年06月06日 08:37:46
  **/
@@ -68,7 +112,8 @@ public class Code01_MergeSort {
         for (int i = 0; i < count; i++) {
             int[] var1 = LogarithmMachine.generateRandomArray(maxSize, maxValue);
             int[] var2 = LogarithmMachine.copyArray(var1);
-            sort(var1);
+//            sort(var1);
+            commonSort(var1);
             LogarithmMachine.sort(var2);
             if (!LogarithmMachine.isEqual(var1, var2)){
                 success = false;
@@ -117,6 +162,37 @@ public class Code01_MergeSort {
         }
         for (int j = 0; j < help.length; j++){
             arr[l + j] = help[j];
+        }
+    }
+
+    public static void commonSort(int[] arr){
+        if (arr == null || arr.length == 0){
+            return;
+        }
+        //步长
+        int stepLength = 1;
+        int n = arr.length;
+        //步长小于N，超过N结束排序
+        while (stepLength < n){
+            int l = 0;
+            //L（左组第一个位置）小于N，超过N本次步长结束
+            while (l < n){
+                int m = l + stepLength - 1;
+                //M（左组最后一个位置）大于等于N，跳过，说明左组不够
+                if (m >= n){
+                    break;
+                }
+                //R（右组最后一个位置） = M + 1 + 步长  或者 N - 1(不够)
+                int r = Math.min(n - 1, m + stepLength);
+                merge(arr, l, m, r);
+                //l来到下一组的位置
+                l = r + 1;
+            }
+            //防止溢出 stepLength * 2 可能超过int的最大值
+            if (stepLength > n/2){
+                break;
+            }
+            stepLength <<= 1;
         }
     }
 
